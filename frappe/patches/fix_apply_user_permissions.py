2d52fc502e1223b973736e3d7677224a32849089
Yain Shurrab <yshurrab@gmail.com>
2019-04-11 15:29:37 +0000

    update patch

--- frappe/patches/fix_apply_user_permissions.py
+++ frappe/patches/fix_apply_user_permissions.py
@@ -19,4 +19,19 @@ def execute():
 	try:
 		frappe.db.sql("""ALTER TABLE`tabUser Permission` add skip_for_doctype INT NULL DEFAULT '0';""")
 	except: pass
+        try:
+		frappe.db.sql("""ALTER TABLE `__Auth` add `salt` VARCHAR(140);""")
+	except: pass
+        try:
+                frappe.db.sql("""ALTER TABLE `tabSystem Settings` add `allow_consecutive_login_attempts` INT DEFAULT '0';""")
+        except: pass
+        try:
+                frappe.db.sql("""ALTER TABLE `tabSystem Settings` add `two_factor_authentication` INT DEFAULT '0';""")
+        except: pass
+        try:
+                frappe.db.sql("""ALTER TABLE `tabSystem Settings` add `allow_login_after_fail` INT DEFAULT '0';""")
+        except: pass
+        try:
+                frappe.db.sql("""ALTER TABLE tabItem add closing_price FLOAT NULL DEFAULT "0";""")
+        except: pass
 
